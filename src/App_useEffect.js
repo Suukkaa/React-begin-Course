47634821b931f7969f6fffbc6ba74196b04f697b
Suukkaa <suokkaa@gmail.com>
2021-12-17 15:43:43 +0900

    react basic

---
+++ src/App_useEffect.js
@@ -0,0 +1,33 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("i run all the time");
+
+  useEffect(() => {
+    console.log("i run only once");
+  }, []);
+  useEffect(() => {
+    console.log("i run when 'keyword' changes");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("i run when 'counter' changes");
+  }, [counter]);
+  useEffect(() => {
+    console.log("i run when keyword & counter changes");
+  }, [counter, keyword]);
+  return (
+    <div>
+      <input onChange={onChange} type="text" placeholder="Search here..." />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click</button>
+    </div>
+  );
+}
+
+export default App;
